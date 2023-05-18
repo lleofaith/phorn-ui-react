280d66c03ff394d858876aa97a15ebf2d1f97be1
lleofaith <mkhembrom13@gmail.com>
2023-05-18 14:55:19 +0530

    light box issue

--- src/pages/detailPage.jsx
+++ src/pages/detailPage.jsx
@@ -11,23 +11,24 @@ const DetailPage = () => {
     const { id } = useParams();
     const titleName = photos.filter((item) => item.id === id)[0]?.title;
     const groupPhoto = photos.filter((item) => item.id === id)[0]?.images;
+    console.log(groupPhoto)
     return (
         <div className='max-w-[1200px] w-full mx-auto h-screen'>
             <h1 className='text-2xl sm:text-4xl text-white m-4 '>{titleName}</h1>
-            <ImageGroup>
-                <div className='mx-4 gap-4 columns-2 sm:columns-4'>
-                    {
-                        groupPhoto?.map((item, index) => (
-                            <Image
-                                key={item}
-                                className="rounded-xl mb-4"
-                                src={item}
-                                alt={item}
-                            />
-                        ))
-                    }
-                </div >
-            </ImageGroup>
+            {/* <ImageGroup> */}
+            <div className='mx-4 gap-4 columns-2 sm:columns-4'>
+                {
+                    groupPhoto?.map((item, index) => (
+                        <img
+                            key={index}
+                            className="rounded-xl mb-4 h-auto w-full"
+                            src={item}
+                            alt={item}
+                        />
+                    ))
+                }
+            </div >
+            {/* </ImageGroup> */}
         </div>
     )
 }
